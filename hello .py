4611476479429923835966ccebc3a01b4b91cd6f
Anubhav bhatt <67511338+anubhavbhatt2000@users.noreply.github.com>
2020-09-10 09:27:21 +0530

    Add files via upload

---
+++ hello .py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+person=input("hi!what is your name?")
+print(" ")
+print("hello!",person,"welcome to python programing.")
+
+
+# In[ ]:
+
+
+
+
